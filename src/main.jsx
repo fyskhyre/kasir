d71cdb13b1b0ebc1da23bf23cb01a0110f472a87
fyskhyre <fyskhyravael@gmail.com>
2026-05-13 10:32:33 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// TAMBAHKAN BARIS INI UNTUK MENGAKTIFKAN PWA
+import 'virtual:pwa-register'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
